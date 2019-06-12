e9067999e79620a23ab1a1bbfa5232b4ace94b9d
kklstyl3 <kkalarasl@gmail.com>
2019-06-12 21:18:51 +0300

    Add Cyberpunk style to Navbar and boxes

--- src/header.js
+++ src/header.js
@@ -8,7 +8,7 @@ class Header extends Component {
     return (
         <div style={{ backgroundColor: '#2f77ce', textAlign: 'center', height: '60px', display: 'flex' }}>
             <NavLink to="/"><img src={wmw} alt="wmw" width="100" height="100" style={{position: "relative", left: "5px"}}/></NavLink>
-            <h1 style={{color: 'white', textShadow: '1px 1px 1px black', marginRight: 'auto', marginLeft: 'auto', width: 'auto', position: 'relative', right: '55px', bottom: '10px'}}>..::World's Most Wanted::..</h1>
+            <h1 style={{color: 'white', textShadow: '2px 2px black', marginRight: 'auto', marginLeft: 'auto', width: 'auto', position: 'relative', right: '55px', bottom: '10px'}}>..::World's Most Wanted::..</h1>
         </div>
     );
     }
